d6b28e00b1b0eb124606fe6607a40402d32dacfb
Stefan Ihle <stefanihle@gmx.net>
2022-08-05 12:27:49 +0200

    create authentication workflow with gooogle as auth provider

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -21,7 +21,7 @@ const firebaseApp = initializeApp(firebaseConfig);
 
 const provider = new GoogleAuthProvider();
 provider.setCustomParameters({
-	propmt: 'select_account'
+	prompt: 'select_account'
 });
 
 export const auth = getAuth();
@@ -30,9 +30,23 @@ export const db = getFirestore();
 
 export const createUserDocumentFromAuth = async userAuth => {
 	const userDocRef = doc(db, 'users', userAuth.uid);
-	console.log(userDocRef);
 
 	const userSnapshot = await getDoc(userDocRef);
-	console.log(userSnapshot);
-	console.log(userSnapshot.exists());
+
+	if (!userSnapshot.exists()) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+
+		try {
+			await setDoc(userDocRef, {
+				displayName,
+				email,
+				createdAt
+			});
+		} catch (error) {
+			console.log('error creating the user', error.message);
+		}
+	}
+
+	return userDocRef;
 };
