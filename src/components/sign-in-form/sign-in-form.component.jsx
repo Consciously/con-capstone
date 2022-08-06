f1e31c4b4fad2863081fe301477788be0c88c11a
Stefan Ihle <stefanihle@gmx.net>
2022-08-06 18:03:33 +0200

    centralized user object into user context component

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,10 +1,9 @@
-import { useState, useContext } from 'react';
+import { useState } from 'react';
 import {
 	signInWithGooglePopup,
 	createUserDocumentFromAuth,
 	signInAuthUserWithEmailAndPassword
 } from '../../utils/firebase/firebase.utils';
-import { UserContext } from '../../contexts/user.context';
 import FormInput from '../form-input/form-input.component';
 import Button from '../button/button.component';
 import './sign-in-form.styles.scss';
@@ -19,15 +18,12 @@ const SignInForm = () => {
 
 	const { email, password } = formFields;
 
-	const { setCurrentUser } = useContext(UserContext);
-
 	const resetFormFields = () => {
 		setFormFields(defaultFormFields);
 	};
 
 	const signInWithGoogle = async () => {
-		const { user } = await signInWithGooglePopup();
-		await createUserDocumentFromAuth(user);
+		await signInWithGooglePopup();
 	};
 
 	const handleSubmit = async event => {
@@ -38,8 +34,6 @@ const SignInForm = () => {
 				email,
 				password
 			);
-
-			setCurrentUser(user);
 			resetFormFields();
 		} catch (error) {
 			switch (error.code) {
