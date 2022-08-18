3a81184571906c76d16d24ccd5062ed7e2f2c484
Stefan Ihle <stefanihle@gmx.net>
2022-08-18 13:04:28 +0200

    bring user to redux-saga

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
-import {
-	signInWithGooglePopup,
-	createUserDocumentFromAuth,
-	signInAuthUserWithEmailAndPassword
-} from '../../utils/firebase/firebase.utils';
+import { useDispatch } from 'react-redux';
 import FormInput from '../form-input/form-input.component';
 import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 import { SignUpContainer, H2, ButtonContainer } from './sign-in-form.styles';
+import {
+	googleSignInStart,
+	emailSignInStart
+} from '../../store/user/user.action';
 
 const defaultFormFields = {
 	email: '',
@@ -14,6 +14,7 @@ const defaultFormFields = {
 };
 
 const SignInForm = () => {
+	const dispatch = useDispatch();
 	const [formFields, setFormFields] = useState(defaultFormFields);
 
 	const { email, password } = formFields;
@@ -23,17 +24,14 @@ const SignInForm = () => {
 	};
 
 	const signInWithGoogle = async () => {
-		await signInWithGooglePopup();
+		dispatch(googleSignInStart());
 	};
 
 	const handleSubmit = async event => {
 		event.preventDefault();
 
 		try {
-			const { user } = await signInAuthUserWithEmailAndPassword(
-				email,
-				password
-			);
+			dispatch(emailSignInStart(email, password));
 			resetFormFields();
 		} catch (error) {
 			switch (error.code) {
