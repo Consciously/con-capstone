448d76c9a4da32ee957f8f5450a4ed1b54963fc7
Stefan Ihle <stefanihle@gmx.net>
2022-09-01 16:20:40 +0200

    typed out payment component

--- src/routes/category/category.component.tsx
+++ src/routes/category/category.component.tsx
@@ -9,8 +9,14 @@ import ProductCard from '../../components/product-card/product-card.component';
 import Spinner from '../../components/spinner/spinner.component';
 import { CategoryContainer, CategoryTitle } from './category.styles';
 
+type CategoryRouteParams = {
+	category: string;
+};
+
 const Category = () => {
-	const { category } = useParams();
+	const { category } = useParams<
+		keyof CategoryRouteParams
+	>() as CategoryRouteParams;
 	console.log('render/re-rendering category component');
 	const categoriesMap = useSelector(selectCategoriesMap);
 	const isLoading = useSelector(selectCategoriesIsLoading);
