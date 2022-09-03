9b3d91529bdc9509855aaecd9984e209490cfbbb
Stefan Ihle <stefanihle@gmx.net>
2022-09-03 16:20:09 +0200

    integrate pwa

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { stripePromise } from './utils/stripe/stripe.utils';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -30,4 +30,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-serviceWorker.register();
+serviceWorkerRegistration.register();
